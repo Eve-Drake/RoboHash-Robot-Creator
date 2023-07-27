e88d757b57bb73a72c4f5af5648421c5df1e560b
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-07-27 15:32:51 +0100

    linking CSS

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const robotName = document.getElementById('robotName')
-const robotDisplay = document.getElementById('robotDisplay')
+const robotDisplay = document.getElementById('robot-display')
 
 function addRobot(){
   if(robotName.value !== ''){
     let newRobot = document.createElement('img')
     newRobot.src = `https://robohash.org/${robotName.value}`
+    newRobot.className = 'robot'
+    newRobot.alt = `Robot Named ${robotName.value}`
     robotDisplay.appendChild(newRobot)
     robotName.value = ''
   }
