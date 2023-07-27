fb8e1eab1a36c99fdf7774a887fda6c9c9e534ba
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-07-27 15:25:30 +0100

    Added the basic parts

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const robotName = document.getElementById('robotName')
+const robotDisplay = document.getElementById('robotDisplay')
 
 function addRobot(){
+  if(robotName.value !== ''){
+    let newRobot = document.createElement('img')
+    newRobot.src = `https://robohash.org/${robotName.value}`
+    robotDisplay.appendChild(newRobot)
+    robotName.value = ''
+  }
   
 }
